be68333ae532f3d255ee0ce38cd7559f8af2dc1e
Aman0092005 <amansinghss009@gmail.com>
2025-08-11 21:05:19 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Name Letter Counter
+
+A simple Express.js and EJS-based web app where you can enter your first and last name, and it will count the total number of letters in your name.
+
+## How it works
+1. Enter your first and last name in the form.
+2. Click Ok.
+3. See the total letter count displayed instantly.
---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+var data = {};
+app.get("/", (req,res) => {
+    res.render("index.ejs",data);
+});
+
+
+app.post("/submit", (req,res) => {
+    data = {count: req.body.fName.length + req.body.lName.length};
+    res.redirect("/");
+});
+
+
+app.listen(port, () => {
+    console.log(`Listening at Port ${port}`);
+});
